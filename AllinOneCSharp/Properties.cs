304748418cea0eb2c2fbcbb38164fc9fb0300513
techiesalman <techiesalman@gmail.com>
2023-01-21 13:00:20 +0530

    Property file

--- AllinOneCSharp/Properties.cs
+++ AllinOneCSharp/Properties.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace AllinOneCSharp
 {
@@ -26,8 +27,9 @@ namespace AllinOneCSharp
         private string _name;
         private int _passmark = 35;
 
+        //Proctecting Class fields using Setter and Getter Method.
 
-        //Student Id cannot be negative so use the conditions
+        //Student Id cannot be negative so use the below conditions
 
         public void setID(int id)
         {
@@ -64,8 +66,98 @@ namespace AllinOneCSharp
         {
             return this._passmark;
         }
-
         //Only get is used in this method to avoid changing of Pass Mark
+
+
+        //Getting rid of Setter and Getter Method
+
+        //Set is used to Write the Property/Method
+        //Get is used to Read the Property/Method;
+
+
+        //Using Property to protect the Class field.
+
+        //For Id
+        public int Id
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Student Id cannot be Zero or Negative");
+                }
+                this._id = value;
+            }
+            get
+            {
+                return this._id;
+            }
+        }
+
+        //For Name
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name cannot be null or empty");
+                }
+                this._name = value;
+            }
+
+            get
+            {
+                return string.IsNullOrEmpty(this._name) ? "No Name Mentioned" : this._name;
+            }
+        }
+
+        //For PassMark
+
+        public int passMark
+        {
+            get
+            {
+                return this._passmark;
+            }
+        }
+
+        //Auto Implemented Properties
+
+        //If the Properties do not have any logic or just need to assign the value then use Auto Implemented Properties
+        //In Auto Implemented Properties, we dont need to assign Class-Field (private).
+
+
+        //Example
+
+        public string Email { get; set; }
+
+        public string City { get; set; }
+
+        //public string Email
+        //{
+        //    set
+        //    {
+        //        this.Email = value;
+        //    }
+        //    get
+        //    {
+        //        return this.Email;
+        //    }
+        //}
+
+        //public string City
+        //{
+        //    get
+        //    {
+        //        return this.City;
+        //    }
+        //    set
+        //    {
+        //        this.City = value;
+        //    }
+        //}
+
 
     }
 }
