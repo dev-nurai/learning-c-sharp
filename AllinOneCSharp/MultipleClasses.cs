04b5edbd9e5e5ac881596dd61734494216258d1a
nurai <coder.nurai@gmail.com>
2023-01-23 23:47:34 +0530

    Create Multiple Classes using Inteface

---
+++ AllinOneCSharp/MultipleClasses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+    interface InterfaceA
+    {
+        void MethodA();
+    }
+    class ClassA : InterfaceA
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("Print Method A");
+        }
+    }
+
+    interface InterfaceB
+    {
+        void MethodB();
+    }
+
+    class ClassB : InterfaceB
+    {
+        public void MethodB()
+        {
+            Console.WriteLine("Print Method B");
+        }
+    }
+
+    class ClassAB : InterfaceB, InterfaceA 
+    {
+        ClassA classa = new ClassA();
+        ClassB classb = new ClassB();
+
+        public void MethodA()
+        {
+            classa.MethodA();
+        }
+        public void MethodB()
+        {
+            classb.MethodB();
+        }
+    }
+
+
+}
