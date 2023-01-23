a6d587c2b83820572a7958e332f2c25ed4e1e124
nurai <coder.nurai@gmail.com>
2023-01-23 20:03:36 +0530

    Diamond Problem Explain

---
+++ AllinOneCSharp/ProblemsofInheritance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+
+    //Diamond Problem
+    class A
+    {
+        public virtual void Print() //Base Class
+        {
+            Console.WriteLine("A is Implemented");
+        }
+
+    }
+    class B : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("A is Implemented");
+        }
+    }
+    class C : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("C is Implemented");
+        }
+    }
+    class D : B //, A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("D is Implemented");
+        }
+    }
+}
+//Git karna baki hai
