bc795b6afc66f557ac0925e050fd0cc131c845c9
nurai <coder.nurai@gmail.com>
2023-01-23 17:34:53 +0530

    Difference between Abstract vs Interface

---
+++ AllinOneCSharp/AbstractvsInterface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+    public abstract class AbstractvsInterface //Can inheritance with Abstract Class or Interface Class
+    {
+        public int Number; //Fields are allowed in Abstract Class
+        public string Name = "Gufran";
+        public void PrintAbstract() //Implementations are allowed with non-abstract class (withinn Abstract Class).
+        {
+            Console.WriteLine("Print of class in within Abstract Class");
+        }
+    }
+
+    public interface IInterface //Interface Class will only inheritance will another Interface class [not others]
+    {
+        //Fields are not allowed in Abstract Class
+        void PrintInterface(); //Cannot have implementation
+    }
+
+}
+
+
+/*
+ Abstract class members can have access modifiers where as Interface members cannot have access modifiers.
+ */
