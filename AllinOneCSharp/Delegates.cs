db779c90c6f4b14bf6eb57dcfb74d754107530b0
nurai <coder.nurai@gmail.com>
2023-01-24 10:29:18 +0530

    Learn Basics of Delegates

---
+++ AllinOneCSharp/Delegates.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+   
+    public delegate void DelegatesMessage(string UsersMessage);
+    //class Delegates //Refer Program.cs
+    //{
+    //public static void DelegatesMessage(string usersMessage)
+    //{
+    //    Console.WriteLine(usersMessage);
+    //}
+    //static void DelegatesMessage(string usersMessage)
+    //{
+    //    Console.WriteLine(usersMessage);
+    //}
+    //    }
+
+
+    delegate bool IsPromotable(TCSEmployee empl);
+    class TCSEmployee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+        public static void PromoteEmployee(List<TCSEmployee> employeesList, IsPromotable isEligibleToPromote)
+        {
+            foreach(TCSEmployee employee in employeesList)
+            {
+                if(isEligibleToPromote(employee)) //If(employee.Experience >=5) Hard Code
+                {
+                    Console.WriteLine(employee.Name + " promoted");
+                }
+            }
+        }
+    }
+
+}
+
+    /*
+     Delegates are type safe function pointer.
+    Same Signature are allowed (void or any but same, also parameters)
+
+    Use delegrate when we need function as parameter.
+
+    >> Lamda Expression
+     */
