3720b76edd859361e197b38d616b8a74475c4fbf
nurai <coder.nurai@gmail.com>
2023-01-24 17:36:24 +0530

    Learn Exception Handing in C Sharp

---
+++ AllinOneCSharp/Exception-Handling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AllinOneCSharp
+{
+    //Checkout the Program.cs file.
+    //If there is an exception or file missing error then user will see Exception Handling message.
+    //Exception Handling message contain value message to the hacker. Hacker can use the message to hack your System.
+}
+/*
+ try block - The code that can possibly cause an exception will be in try block.
+catch block - Handles the exceptions.
+finally block - Clasn and free resources that the class was holding onto during the program execution. Finally block is optional.
+ 
+StreamReader is used to Read the file.
+
+
+ */
