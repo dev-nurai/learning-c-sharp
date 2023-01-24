c55a2815110632e7feae7aa8083111b19594f35e
nurai <coder.nurai@gmail.com>
2023-01-25 01:16:10 +0530

    Learn Enum

---
+++ AllinOneCSharp/Enums.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+    public class Client
+    {
+        //Default underlying type is "int" and value starts from Zero.
+        public enum Gender : short //Short is used to save memory space.
+        {
+            Unknow, // = 0,
+            Male, // = 1,
+            Female // = 2
+        }
+        //Enums values need not be in sequential order Any valid underlying type value is allowed
+        /*
+         {  Unknown = 1
+            Male = 44,
+            Female = 99
+         }
+
+         
+         */
+        public enum Season
+        {
+            Winter,
+            Summer,
+            Rainy
+        }
+
+        //0 - Unknown
+        //1 - Male
+        //2 - Female
+        public string Name { get; set; }
+        public Gender gender { get; set; }
+
+    }
+}
+
+//Enums are strongly typed constants.
+
+/*
+If program uses set of integral numbers, consider replacing them with "Enums", which makes Program more
+    Readable [code]
+    Maintainable
+
+*/
