a6ad21cfd5672abb29c9b2544660d498135d1f59
nurai <coder.nurai@gmail.com>
2023-01-26 20:33:09 +0530

    Learn Generics

---
+++ AllinOneCSharp/Generics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+    internal class Generics
+    {
+    }
+
+    public class EqualCalculator//<T>  //Class Generic
+    {
+        //public static bool AreEqual(int Value1, int Value2) //Fails if the user inputs string value
+        //public static bool AreEqual(object Value1, object Value2)
+        public static bool AreEqual<T>(T Value1, T Value2) //Method Generic
+        {
+            //return Value2 == Value2;
+
+            return Value1.Equals(Value2);
+        }
+    }
+}
+
+/*
+    If the input is Value Type than it will converted in Reference Type in Object, later will be campared.
+    Converting "Value Type" to "Reference Type" is called Boxing.
+    Every step Boxing will be done. So, efficiency will drop while using the object.
+    
+    Generics are use when dont know the parameters Type (int or String) and later they are used. So, we use Generics in Method or Class
+    to avoid using Object.
+ 
+ */
