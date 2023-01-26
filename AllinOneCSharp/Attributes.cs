8772e2603341e8300c1c453beefd2536abf5f624
nurai <coder.nurai@gmail.com>
2023-01-26 15:32:44 +0530

    Learn Attributes

---
+++ AllinOneCSharp/Attributes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace AllinOneCSharp
+{
+
+    public class Calculator
+    {
+        [Obsolete("Use Add(List<int>Numbers)")]
+        public static int CalAdd(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        //If Users have more than two numbers to add
+
+        public static int CalAdd(List<int> Numbers)
+        {
+            int SumAll = 0;
+
+            foreach (int num in Numbers)
+            {
+                SumAll += num;
+            }
+            return SumAll;
+        }
+    }
+        
+    
+}
