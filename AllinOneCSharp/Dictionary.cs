04bfba24aa4937273e89a189f90b4adcec129cae
nurai <coder.nurai@gmail.com>
2023-01-28 10:31:18 +0530

    Learn Dictionary

---
+++ AllinOneCSharp/Dictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+    public class Farmer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Earning { get; set; }
+    }
+}
+
+/*
+ Dictionary uses Key and Values Pair
+    - Keys must be unique to the given objects.
+
+ Methods of Dictionary Class 
+    - TryGetType()
+    - Count()
+    - Remove()
+    - Clear()
+    - Using LINQ extension methods with Dictionary
+    - Ways to convert an arrays into dictionary
+ 
+ */
