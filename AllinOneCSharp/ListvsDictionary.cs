d31dc72d1dbb9f7fc01aea3e07eeb32c7659423d
nurai <coder.nurai@gmail.com>
2023-01-30 17:09:26 +0530

    Learn when to use Dictionary over List

---
+++ AllinOneCSharp/ListvsDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+    internal class Country
+    {
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string Capital { get; set; }
+
+    }
+}
