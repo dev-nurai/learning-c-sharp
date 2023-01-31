9e5869ef0c5bebdcacb1e901d116bb40cac31b86
nurai <coder.nurai@gmail.com>
2023-01-31 22:56:32 +0530

    Learn about Threads in C#

---
+++ AllinOneCSharp/Threads.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllinOneCSharp
+{
+    internal class Threads
+    {
+
+
+        //public static void EvenNumbersSum()
+        //{
+        //    double sum = 0;
+        //    for(int i = 0; i < 5000000; i++)
+        //    {
+        //        if(i % 2 == 0)
+        //        {
+        //            sum = sum + i;
+        //        }
+        //    }
+        //    Console.WriteLine("Sum of EvenNumbers = {0}", sum);
+        //}
+        //public static void OddNumbersSum()
+        //{
+        //    double sum = 0;
+        //    for (int i = 0; i < 5000000; i++)
+        //    {
+        //        if (i % 2 == 1)
+        //        {
+        //            sum = sum + i;
+        //        }
+        //    }
+        //    Console.WriteLine("Sum of EvenNumbers = {0}", sum);
+        //}
+
+    }
+}
